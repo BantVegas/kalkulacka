96bd6350dd21f5198266f8da7b5497a329ec899e
lukac <lukacm14@gmail.com>
2025-06-11 19:34:48 +0200

    Pridaná dynamická sitemap.xml ako Next.js route handler

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -2,6 +2,14 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const baseUrl = "https://univerzalkalkulacka.sk";
+  const staticPages = [
+    "",
+    "danova-kalkulacka",
+    "uverova-kalkulacka",
+    "investicna-kalkulacka",
+    "poistna-kalkulacka",
+    "o-nas",
+  ];
   const clanky = [
     "ako-sa-vypocita-cista-a-hruba-mzda",
     "ako-funguje-poistenie-na-slovensku",
@@ -9,10 +17,18 @@ export async function GET() {
     "ako-zacat-investovat",
     "ako-usetrit-na-daniach",
   ];
-  const urls = clanky.map(slug =>
-    `<url><loc>${baseUrl}/blog/${slug}</loc></url>`
-  ).join("");
+
+  const urls = [
+    ...staticPages.map(page =>
+      `<url><loc>${baseUrl}/${page}</loc></url>`
+    ),
+    ...clanky.map(slug =>
+      `<url><loc>${baseUrl}/blog/${slug}</loc></url>`
+    ),
+  ].join("");
+
   const body = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
+
   return new NextResponse(body, {
     headers: { "Content-Type": "application/xml" },
   });
